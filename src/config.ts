b183e7f9d661981a6c997e05a09c72f048dfb20b
Huan LI <zixia@zixia.net>
2018-07-11 19:53:26 +0800

    add export VERSION

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,25 @@
+// tslint:disable:no-var-requires
+
 import {
   // Brolog,
   log,
 }               from 'brolog'
 
+/**
+ * VERSION
+ */
+let VERSION: string = '0.0.0'
+try {
+  VERSION = require('../../package.json').version
+} catch (e) {
+  VERSION = require('../package.json').version
+}
+
+/**
+ * exports
+ */
 export {
+  VERSION,
   log,
 }
 // export const log = new Brolog()
