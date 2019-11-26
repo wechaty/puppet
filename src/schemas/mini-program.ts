d95ddc9a7ab1041282b0da4e8780c71ffe575541
su-chang <su_chang@yeah.net>
2019-11-26 19:21:22 +0800

    modify: @deprecated for older variable names

--- src/schemas/mini-program.ts
+++ src/schemas/mini-program.ts
@@ -1,7 +1,9 @@
 export interface MiniProgramPayload {
     appid?          : string,    // optional, appid, get from wechat (mp.weixin.qq.com)
     description?    : string,    // optional, mini program title
+    pagepath?       : string,    // @deprecated
     pagePath?       : string,    // optional, mini program page path
+    thumbnailurl?   : string,    // @deprecated
     thumbUrl?       : string,    // optional, default picture, convert to thumbnail
     title?          : string,    // optional, mini program title
     username?       : string,    // original ID, get from wechat (mp.weixin.qq.com)
