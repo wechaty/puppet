c2380bd1214a124ad6c94a1745a24d575bfc7bcc
SuperChang <su_chang@yeah.net>
2019-11-28 11:14:58 +0800

    Add thumbkey for MiniProgram payload. (#65)
    * modify: add thumbkey for MiniProgram payload.
    
    * modify: bump version
    
    * modify: change variable names to be better.
    
    * modify: @deprecated for older variable names
    
    * remove deprecated keys

--- src/schemas/mini-program.ts
+++ src/schemas/mini-program.ts
@@ -1,8 +1,9 @@
 export interface MiniProgramPayload {
     appid?          : string,    // optional, appid, get from wechat (mp.weixin.qq.com)
     description?    : string,    // optional, mini program title
-    pagepath?       : string,    // optional, mini program page path
-    thumbnailurl?   : string,    // optional, default picture, convert to thumbnail
+    pagePath?       : string,    // optional, mini program page path
+    thumbUrl?       : string,    // optional, default picture, convert to thumbnail
     title?          : string,    // optional, mini program title
     username?       : string,    // original ID, get from wechat (mp.weixin.qq.com)
+    thumbKey?       : string,    // original, thumbnailurl and thumbkey will make the headphoto of mini-program better
 }
