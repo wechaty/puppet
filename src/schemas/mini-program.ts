39a8cc7b881878d5c4e45c325a63df9cd403a0d0
su-chang <su_chang@yeah.net>
2019-11-28 09:21:21 +0800

    remove deprecated keys

--- src/schemas/mini-program.ts
+++ src/schemas/mini-program.ts
@@ -1,9 +1,7 @@
 export interface MiniProgramPayload {
     appid?          : string,    // optional, appid, get from wechat (mp.weixin.qq.com)
     description?    : string,    // optional, mini program title
-    pagepath?       : string,    // @deprecated
     pagePath?       : string,    // optional, mini program page path
-    thumbnailurl?   : string,    // @deprecated
     thumbUrl?       : string,    // optional, default picture, convert to thumbnail
     title?          : string,    // optional, mini program title
     username?       : string,    // original ID, get from wechat (mp.weixin.qq.com)
