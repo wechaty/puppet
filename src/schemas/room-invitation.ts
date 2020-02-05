1b570d4dd68557d2e01d9863f2b85174f878468d
Huan LI <zixia@zixia.net>
2020-02-05 19:57:12 +0800

    refine room invitation payload

--- src/schemas/room-invitation.ts
+++ src/schemas/room-invitation.ts
@@ -1,8 +1,18 @@
 export interface RoomInvitationPayload {
   id               : string,
   inviterId        : string,
-  roomTopic        : string,
-  roomMemberCount  : number,
-  roomMemberIdList : string[],   // Friends' Contact Id List of the Room
-  timestamp        : number,     // Unix Timestamp, in seconds
+
+  roomTopic        : string,  // deprecated. use topic instead.
+  topic?           : string
+
+  avatar?          : string,
+  invitation?      : string,
+
+  roomMemberCount  : number,  // deprecated. use memberCount instead.
+  memberCount?     : number,
+
+  roomMemberIdList : string[],  // deprecated. use memberIdList instead.
+  memberIdList     : string[],  // Friends' Contact Id List of the Room
+
+  timestamp        : number,     // Unix Timestamp, in seconds or milliseconds
 }
