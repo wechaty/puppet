ccc6f964975bf943d491ad7c5d0c25a5ff411679
Huan LI <zixia@zixia.net>
2020-02-05 19:59:19 +0800

    optional deprecated properties.

--- src/schemas/room-invitation.ts
+++ src/schemas/room-invitation.ts
@@ -1,18 +1,21 @@
+/**
+ * TODO: Huan(202002) clean the deprecated properties
+ */
 export interface RoomInvitationPayload {
   id               : string,
   inviterId        : string,
 
-  roomTopic        : string,  // deprecated. use topic instead.
+  roomTopic?       : string,  // deprecated. use topic instead.
   topic?           : string
 
   avatar?          : string,
   invitation?      : string,
 
-  roomMemberCount  : number,  // deprecated. use memberCount instead.
+  roomMemberCount? : number,  // deprecated. use memberCount instead.
   memberCount?     : number,
 
-  roomMemberIdList : string[],  // deprecated. use memberIdList instead.
-  memberIdList     : string[],  // Friends' Contact Id List of the Room
+  roomMemberIdList?: string[],  // deprecated. use memberIdList instead.
+  memberIdList?    : string[],  // Friends' Contact Id List of the Room
 
   timestamp        : number,     // Unix Timestamp, in seconds or milliseconds
 }
