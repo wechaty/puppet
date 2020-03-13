0817de72647268d93fbb3315833a8fd16da441fb
Huan LI <zixia@zixia.net>
2020-03-13 12:06:16 +0800

    code clean

--- src/schemas/mini-program.ts
+++ src/schemas/mini-program.ts
@@ -1,9 +1,9 @@
 export interface MiniProgramPayload {
-    appid?          : string,    // optional, appid, get from wechat (mp.weixin.qq.com)
-    description?    : string,    // optional, mini program title
-    pagePath?       : string,    // optional, mini program page path
-    thumbUrl?       : string,    // optional, default picture, convert to thumbnail
-    title?          : string,    // optional, mini program title
-    username?       : string,    // original ID, get from wechat (mp.weixin.qq.com)
-    thumbKey?       : string,    // original, thumbnailurl and thumbkey will make the headphoto of mini-program better
+    appid?       : string,   // optional, appid, get from wechat (mp.weixin.qq.com)
+    description? : string,   // optional, mini program title
+    pagePath?    : string,   // optional, mini program page path
+    thumbUrl?    : string,   // optional, default picture, convert to thumbnail
+    title?       : string,   // optional, mini program title
+    username?    : string,   // original ID, get from wechat (mp.weixin.qq.com)
+    thumbKey?    : string,   // original, thumbnailurl and thumbkey will make the headphoto of mini-program better
 }
