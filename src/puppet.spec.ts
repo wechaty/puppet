7a67758e2927d1fd07cee395b0133e1c12b78a64
Huan LI <zixia@zixia.net>
2020-04-02 11:38:32 +0800

    refactor PuppetTest to seprate module directory (#89)

--- src/puppet.spec.ts
+++ src/puppet.spec.ts
@@ -6,196 +6,29 @@ import test  from 'blue-tape'
 import sinon from 'sinon'
 
 import {
-  FileBox,
   MemoryCard,
 }                 from './config'
 
 import {
   ContactGender,
   ContactPayload,
-  ContactPayloadFilterFunction,
-  ContactQueryFilter,
   ContactType,
 }                                 from './schemas/contact'
 import {
-  FriendshipPayload,
-}                                 from './schemas/friendship'
-import {
-  ImageType,
-}                                 from './schemas/image'
-import {
   MessagePayload,
-  MessagePayloadFilterFunction,
   MessageQueryFilter,
   MessageType,
 }                                 from './schemas/message'
 import {
-  RoomMemberPayload,
   RoomPayload,
-  RoomPayloadFilterFunction,
-  RoomQueryFilter,
 }                                 from './schemas/room'
+
+/**
+ * The Fixture
+ */
 import {
-  RoomInvitationPayload,
-}                                 from './schemas/room-invitation'
-import {
-  UrlLinkPayload,
-}                                 from './schemas/url-link'
-import {
-  MiniProgramPayload,
-}                                 from './schemas/mini-program'
-
-import {
-  Puppet,
-}                                 from './puppet'
-
-class PuppetTest extends Puppet {
-
-  public async start () : Promise<void> { return {} as any }
-  public async stop ()  : Promise<void> { return {} as any }
-
-  public async ding (data?: string)   : Promise<void> { return data as void }
-  public async logout () : Promise<void> { return {} as any }
-
-  /**
-   *
-   * ContactSelf
-   *
-   *
-   */
-  public async contactSelfQRCode ()                     : Promise<string> { return '' }
-  public async contactSelfName (name: string)           : Promise<void> { return void name }
-  public async contactSelfSignature (signature: string) : Promise<void> { return void signature }
-
-  /**
-   *
-   * Tag
-   *
-   */
-  public async tagContactAdd (tagId: string, contactId: string) : Promise<void> { return void { contactId, tagId } }
-  public async tagContactRemove (tagId: string, contactId: string) : Promise<void> { return void { contactId, tagId } }
-  public async tagContactDelete (tagId: string) : Promise<void> { return void { tagId } }
-  public async tagContactList (contactId?: string) : Promise<string[]> { return [contactId || ''] }
-
-  /**
-   *
-   * Contact
-   *
-   */
-  public async contactAlias (contactId: string)                        : Promise<string>
-  public async contactAlias (contactId: string, alias: string | null)  : Promise<void>
-  public async contactAlias (contactId: string, alias?: string | null) : Promise<string | void> { return { alias, contactId } as any }
-
-  public async contactAvatar (contactId: string)                 : Promise<FileBox>
-  public async contactAvatar (contactId: string, file: FileBox)  : Promise<void>
-  public async contactAvatar (contactId: string, file?: FileBox) : Promise<void | FileBox> { return { contactId, file } as any }
-
-  public async contactList ()                    : Promise<string[]> { return {} as any }
-
-  public async contactRawPayload (id: string)            : Promise<any> { return { id } as any }
-  public async contactRawPayloadParser (rawPayload: any) : Promise<ContactPayload> { return { rawPayload } as any }
-
-  /**
-   *
-   * Friendship
-   *
-   */
-  public async friendshipRawPayload (id: string)            : Promise<any> { return { id } as any }
-  public async friendshipRawPayloadParser (rawPayload: any) : Promise<FriendshipPayload> { return rawPayload }
-
-  public async friendshipSearchPhone (phone: string) : Promise<null | string> { return phone }
-  public async friendshipSearchWeixin (weixin: string) : Promise<null | string> { return weixin }
-  public async friendshipAdd (contactId: string, hello?: string) : Promise<void> { return { contactId, hello } as any }
-  public async friendshipAccept (friendshipId: string)           : Promise<void> { return { friendshipId } as any }
-
-  /**
-   *
-   * Message
-   *
-   */
-  public async messageContact     (messageId: string)                       : Promise<string> { return { messageId } as any }
-  public async messageFile        (messageId: string)                       : Promise<FileBox> { return { messageId } as any }
-  public async messageImage       (messageId: string, imageType: ImageType) : Promise<FileBox> { return { imageType, messageId } as any }
-  public async messageMiniProgram (messageId: string)                       : Promise<MiniProgramPayload> { return { messageId } as any }
-  public async messageUrl         (messageId: string)                       : Promise<UrlLinkPayload> { return { messageId } as any }
-
-  public async messageForward         (conversationId: string, messageId: string)                      : Promise<void | string> { return { conversationId, messageId } as any }
-  public async messageSendContact     (conversationId: string, contactId: string)                      : Promise<void | string> { return { contactId, conversationId } as any }
-  public async messageSendFile        (conversationId: string, file: FileBox)                          : Promise<void | string> { return { conversationId, file } as any }
-  public async messageSendText        (conversationId: string, text: string)                           : Promise<void | string> { return { conversationId, text } as any }
-  public async messageSendUrl         (conversationId: string, urlLinkPayload: UrlLinkPayload)         : Promise<void | string> { return { conversationId, urlLinkPayload } as any }
-  public async messageSendMiniProgram (conversationId: string, miniProgramPayload: MiniProgramPayload) : Promise<void | string> { return { conversationId, miniProgramPayload } as any }
-
-  public async messageRawPayload (id: string)            : Promise<any> { return { id } as any }
-  public async messageRawPayloadParser (rawPayload: any) : Promise<MessagePayload> { return { rawPayload } as any }
-
-  public async messageRecall (messageId: string)                : Promise<boolean> { return { messageId } as any }
-
-  public messageQueryFilterFactory (
-    query: MessageQueryFilter,
-  ): MessagePayloadFilterFunction {
-    return super.messageQueryFilterFactory(query)
-  }
-
-  /**
-   *
-   * Room Invitation
-   *
-   */
-  public async roomInvitationAccept (_: string): Promise<void> { }
-
-  public async roomInvitationRawPayload (roomInvitationId: string) : Promise<any> { return { roomInvitationId } as any }
-  public async roomInvitationRawPayloadParser (rawPayload: any)    : Promise<RoomInvitationPayload> { return rawPayload }
-
-  /**
-   *
-   * Room
-   *
-   */
-  public async roomAnnounce (roomId: string)                : Promise<string>
-  public async roomAnnounce (roomId: string, text: string)  : Promise<void>
-  public async roomAnnounce (roomId: string, text?: string) : Promise<void | string> { return { roomId, text } as any }
-
-  public async roomAdd (roomId: string, contactId: string)          : Promise<void> { return { contactId, roomId } as any }
-  public async roomAvatar (roomId: string)                          : Promise<FileBox> { return { roomId } as any }
-  public async roomCreate (contactIdList: string[], topic?: string) : Promise<string> { return { contactIdList, topic } as any }
-  public async roomDel (roomId: string, contactId: string)          : Promise<void> { return { contactId, roomId } as any }
-  public async roomQuit (roomId: string)                            : Promise<void> { return { roomId } as any }
-  public async roomQRCode (roomId: string)                          : Promise<string> { return { roomId } as any }
-
-  public async roomTopic (roomId: string)                 : Promise<string>
-  public async roomTopic (roomId: string, topic: string)  : Promise<void>
-  public async roomTopic (roomId: string, topic?: string) : Promise<string | void> { return { roomId, topic } as any }
-
-  public async roomList ()                     : Promise<string[]> { return {} as any }
-  public async roomMemberList (roomId: string) : Promise<string[]> { return { roomId } as any }
-
-  public async roomRawPayload (id: string)            : Promise<any> { return { id } as any }
-  public async roomRawPayloadParser (rawPayload: any) : Promise<RoomPayload> { return { rawPayload } as any }
-
-  public async roomMemberRawPayload (roomId: string, contactId: string) : Promise<any> { return { contactId, roomId } as any }
-  public async roomMemberRawPayloadParser (rawPayload: any)             : Promise<RoomMemberPayload> { return rawPayload }
-
-  /**
-   * expose to public for internal methods:
-   */
-  public roomQueryFilterFactory (
-    query: RoomQueryFilter,
-  ): RoomPayloadFilterFunction {
-    return super.roomQueryFilterFactory(query)
-  }
-
-  public contactQueryFilterFactory (
-    query: ContactQueryFilter,
-  ): ContactPayloadFilterFunction {
-    return super.contactQueryFilterFactory(query)
-  }
-
-  public reset (reason: string): void {
-    return super.reset(reason)
-  }
-
-}
+  PuppetTest,
+}                                 from '../tests/fixtures/puppet-test/puppet-test'
 
 test('contactQueryFilterFunction()', async t => {
 
@@ -603,4 +436,22 @@ test('messageQueryFilterFactory() two condition', async t => {
   t.equal(resultPayload.length, 1, 'should get one result')
   t.equal(resultPayload[0].text, EXPECTED_TEXT_DATA, 'should get text data')
   t.equal(resultPayload[0].type, EXPECTED_TYPE_URL, 'should get type url')
+})
+
+test('name()', async t => {
+  const puppet = new PuppetTest()
+
+  const name = puppet.name()
+  const EXPECTED_NAME = 'puppet-test'
+
+  t.equal(name, EXPECTED_NAME, 'should get the child class package name')
+})
+
+test('version()', async t => {
+  const puppet = new PuppetTest()
+
+  const version = puppet.version()
+  const EXPECTED_VERSION = '1.0.0'
+
+  t.equal(version, EXPECTED_VERSION, 'should get the child class package version')
 })
