e298ad465f1c7258815e1aa66071a9c6fbce7440
padlocal <oxddoxdd@gmail.com>
2021-05-14 16:16:16 +0800

    align code idention

--- src/schemas/message.ts
+++ src/schemas/message.ts
@@ -48,30 +48,30 @@ export enum WechatAppMessageType {
  *  Huan(202001): The Windows(PC) DLL match the following numbers.
  */
 export enum WechatMessageType {
-  Text              = 1,
-  Image             = 3,
-  Voice             = 34,
-  VerifyMsg         = 37,
-  PossibleFriendMsg = 40,
-  ShareCard         = 42,
-  Video             = 43,
-  Emoticon          = 47,
-  Location          = 48,
-  App               = 49,
-  VoipMsg           = 50,
-  StatusNotify      = 51,
-  VoipNotify        = 52,
-  VoipInvite        = 53,
-  MicroVideo        = 62,
+  Text                = 1,
+  Image               = 3,
+  Voice               = 34,
+  VerifyMsg           = 37,
+  PossibleFriendMsg   = 40,
+  ShareCard           = 42,
+  Video               = 43,
+  Emoticon            = 47,
+  Location            = 48,
+  App                 = 49,
+  VoipMsg             = 50,
+  StatusNotify        = 51,
+  VoipNotify          = 52,
+  VoipInvite          = 53,
+  MicroVideo          = 62,
   VerifyMsgEnterprise = 65,
-  Transfer          = 2000, // 转账
-  RedEnvelope       = 2001, // 红包
-  MiniProgram       = 2002, // 小程序
-  GroupInvite       = 2003, // 群邀请
-  File              = 2004, // 文件消息
-  SysNotice         = 9999,
-  Sys               = 10000,
-  Recalled          = 10002,  // NOTIFY 服务通知
+  Transfer            = 2000, // 转账
+  RedEnvelope         = 2001, // 红包
+  MiniProgram         = 2002, // 小程序
+  GroupInvite         = 2003, // 群邀请
+  File                = 2004, // 文件消息
+  SysNotice           = 9999,
+  Sys                 = 10000,
+  Recalled            = 10002,  // NOTIFY 服务通知
 }
 
 /** @hidden */
