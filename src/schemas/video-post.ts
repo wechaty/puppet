5272bc35691b8cb271a19ed6aa7d74265a43e1dd
su-chang <su_chang@yeah.net>
2021-09-08 19:44:12 +0800

    fix: style

--- src/schemas/video-post.ts
+++ src/schemas/video-post.ts
@@ -2,8 +2,8 @@
  * weixin VideoPost(Channels) introduction, see: https://wechatwiki.com/wechat-resources/wechat-channels-short-video-feature-complete-guide/
  */
 export interface VideoPostPayload {
-  authorId?: string,
-  coverageUrl?: string,
-  title: string,
-  videoUrl: string,
+  authorId?    : string,
+  coverageUrl? : string,
+  title        : string,
+  videoUrl     : string,
 }
