47a461e499e2dbc609c16e01c906fa0241e0e097
Huan (李卓桓) <zixia@zixia.net>
2021-10-15 11:41:49 +0800

    clean

--- tests/integration.spec.ts
+++ tests/integration.spec.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm
+// @ts-check
 
 import { test }  from 'tstest'
 
-test('integration testing', async t => {
-  t.pass('ok')
+import { PuppetTest } from './fixtures/puppet-test/puppet-test.js'
+
+test('Integration testing', async t => {
+
+  const puppet = new PuppetTest()
+
+  t.ok(puppet, 'tbw')
 })
