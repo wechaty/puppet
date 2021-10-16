57070ae2e5c05a1f1f50720e8aac8086d2f00374
Huan (李卓桓) <zixia@zixia.net>
2021-10-17 00:39:27 +0800

    add memoryMixin unit test

--- src/mixins/memory-mixin.spec.ts
+++ src/mixins/memory-mixin.spec.ts
@@ -1,12 +1,16 @@
-#!/usr/bin/env -S node --loader ts-node/esm
+#!/usr/bin/env -S node --no-warnings --loader ts-node/esm
 
 import {
   test,
 }           from 'tstest'
+import { PuppetSkelton } from '../puppet/skelton.js'
 
 import type {
   MemoryMixin,
   ProtectedPropertyMemoryMixin,
+}                                   from './memory-mixin.js'
+import {
+  memoryMixin,
 }                                   from './memory-mixin.js'
 
 test('ProtectedPropertyMemoryMixin', async t => {
@@ -15,4 +19,18 @@ test('ProtectedPropertyMemoryMixin', async t => {
 
   const noOneLeft: NotExistTest = true
   t.ok(noOneLeft, 'should match Mixin properties for every protected property')
+})
+
+test('MemoryMixin', async t => {
+  const Test = class extends memoryMixin(PuppetSkelton) {}
+
+  const test = new Test()
+  t.ok(test.memory, 'should has memory')
+  t.notOk(test.memory.name, 'should has no memory name')
+
+  const memoryGet = () => test.memory.get('test')
+
+  await t.rejects(memoryGet, 'should reject get() before memory.load()')
+  await test.start()
+  await t.resolves(memoryGet, 'should resolve get() after memory.load()')
 })
