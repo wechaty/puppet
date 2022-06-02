6764bd027b3074c84996ea1ff8af621745504650
suchang <su_chang@yeah.net>
2022-06-02 16:37:44 +0800

    fix: remove the number in bracket for MessageType

--- src/schemas/message.ts
+++ src/schemas/message.ts
@@ -4,21 +4,21 @@
 export enum MessageType {
   Unknown = 0,
 
-  Attachment  = 1,    // Attach(6),
-  Audio       = 2,    // Audio(1), Voice(34)
-  Contact     = 3,    // ShareCard(42)
-  ChatHistory = 4,    // ChatHistory(19)
-  Emoticon    = 5,    // Sticker: Emoticon(15), Emoticon(47)
-  Image       = 6,    // Img(2), Image(3)
-  Text        = 7,    // Text(1)
-  Location    = 8,    // Location(48)
-  MiniProgram = 9,    // MiniProgram(33)
-  GroupNote   = 10,   // GroupNote(53)
-  Transfer    = 11,   // Transfers(2000)
-  RedEnvelope = 12,   // RedEnvelopes(2001)
-  Recalled    = 13,   // Recalled(10002)
-  Url         = 14,   // Url(5)
-  Video       = 15,   // Video(4), Video(43)
+  Attachment  = 1,    // Attach,
+  Audio       = 2,    // Audio, Voice
+  Contact     = 3,    // ShareCard
+  ChatHistory = 4,    // ChatHistory
+  Emoticon    = 5,    // Sticker: Emoticon, Emoticon
+  Image       = 6,    // Image
+  Text        = 7,    // Text
+  Location    = 8,    // Location
+  MiniProgram = 9,    // MiniProgram
+  GroupNote   = 10,   // GroupNote
+  Transfer    = 11,   // Transfers
+  RedEnvelope = 12,   // RedEnvelopes
+  Recalled    = 13,   // Recalled
+  Url         = 14,   // Url
+  Video       = 15,   // Video
   Post        = 16,   // Moment, Channel, Tweet, etc
   System      = 17,   // System Message
 }
