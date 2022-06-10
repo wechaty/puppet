f6fdc469d6140e8f8e80cbdbe2d5b5f467630564
NickWang <brotherstyx@gmail.com>
2022-06-10 17:06:45 +0800

    fix: 🐛 improve test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,16 +5,21 @@ import { test } from 'tstest'
 
 import { PuppetTest } from './fixtures/puppet-test/puppet-test.js'
 
-test('Integration testing', async t => {
+test('login testing', async t => {
 
   const puppet = new PuppetTest()
 
-  puppet.on('login', _user => {
-    t.pass('should receive a login event')
-  })
-
+  const USER_ID = 'test-user'
+  let userFromEvent = null
+  const future = new Promise<void>(resolve => puppet.on('login', user => {
+    userFromEvent = user.contactId
+    resolve()
+  }))
   await puppet.start()
-  puppet.login('test-user')
+  puppet.login(USER_ID)
+  await future
+
+  t.same(userFromEvent, USER_ID, `should get login event with ${USER_ID}`)
   await puppet.stop()
-  t.ok(!puppet.isLoggedIn, 'puppet should not be logged in after stop')
+  t.notOk(puppet.isLoggedIn, 'puppet should not be logged in after stop')
 })
