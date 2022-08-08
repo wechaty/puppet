5084c89fa4c2d2db5839692a0081bdf71b11e709
NickWang <brotherstyx@gmail.com>
2022-08-08 14:41:29 +0800

    fix: 🐛 var naming

--- src/mixins/cache-mixin.ts
+++ src/mixins/cache-mixin.ts
@@ -115,7 +115,7 @@ const cacheMixin = <MixinBase extends typeof PuppetSkeleton & LoginMixin>(mixinB
         [DirtyType.Post]:         (id: string) => this.cache.post.delete(id),
         [DirtyType.Room]:         (id: string) => this.cache.room.delete(id),
         [DirtyType.RoomMember]:   (id: string) => this.cache.roomMember.delete(id),
-        [DirtyType.Tag]:          (key: string) => this.cache.tag.delete(key),
+        [DirtyType.Tag]:          (id: string) => this.cache.tag.delete(id),
         [DirtyType.TagGroup]:     (id: string) => this.cache.tagGroup.delete(id),
         [DirtyType.Unspecified]:  (id: string) => { throw new Error('Unspecified type with id: ' + id) },
       }
