66e83c017a8b5d1755b6918a7fccb6e2a9b60b1c
NickWang <brotherstyx@gmail.com>
2022-10-08 17:19:34 +0800

    feat: 🎸 moment methods

---
+++ src/mixins/moment-mixin.ts
@@ -0,0 +1,33 @@
+import type { FileBoxInterface } from 'file-box'
+import {
+  log,
+} from '../config.js'
+
+import type { PuppetSkeleton } from '../puppet/mod.js'
+import type { PostPayload } from '../schemas/mod.js'
+import type { PostMixin } from './post-mixin.js'
+
+const momentMixin = <MixinBase extends typeof PuppetSkeleton & PostMixin>(mixinBase: MixinBase) => {
+
+  abstract class MomentMixin extends mixinBase {
+
+    constructor (...args: any[]) {
+      super(...args)
+      log.verbose('PuppetMomentMixin', 'constructor()')
+    }
+
+    // alias for postPublish (feel like it should be part of moment mixin)
+    momentPost (payload: PostPayload): Promise<void | string> {
+      return this.postPublish(payload)
+    }
+
+    // R & W methods
+    abstract momentSignature (text?: string): Promise<void | string>
+    abstract momentCoverage (cover?: FileBoxInterface): Promise<void | FileBoxInterface>
+
+  }
+
+  return MomentMixin
+}
+
+export { momentMixin }
