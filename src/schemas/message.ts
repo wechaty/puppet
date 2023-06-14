c70a9b47a49170e94da7c06c4ead2348dc6dd88f
NickWang <brotherstyx@gmail.com>
2023-06-14 16:08:07 +0800

    Feat/system message wechaty (#213)
    * feat: 🎸 system message
    
    * fix: 🐛 swc version conflict
    
    * fix: 🐛 lint
    
    * fix: 🐛 System type is not supported to say

--- src/schemas/message.ts
+++ src/schemas/message.ts
@@ -20,6 +20,8 @@ export enum MessageType {
   Url         = 14,   // Url(5)
   Video       = 15,   // Video(4), Video(43)
   Post        = 16,   // Moment, Channel, Tweet, etc
+
+  System      = 18,   // System Message
 }
 
 /**
